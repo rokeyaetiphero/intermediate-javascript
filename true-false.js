b7efa8906643336136d462707a3df5b456b02b35
rokeyaetiphero <roserapunzel96@gmail.com>
2020-09-24 02:38:55 +0600

    my-intermediate-commit

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//falsy
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+//truthy
+
+let name = false;
+const age = 4;
+if (age)
+{
+    console.log("condition is true");
+}
+else
+{
+    console.log("condition is false");
+}
